b81fcfb13498b67228f96d82d0792a7f8004ac94
Clément Laballe <clement.laballe@anakeen.com>
2009-08-17 12:02:07 +0200

    Only display 'create mailbox' if user has create permission [closes #9]

--- Actions/admin.php
+++ Actions/admin.php
@@ -6,14 +6,12 @@
  * @version $Id: admin.php,v 1.3 2007/10/22 12:42:10 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
- * @subpackage 
- */
- /**
+ * @subpackage
  */
 
 
 
-include_once("FDL/Lib.Dir.php");
+include_once ("FDL/Lib.Dir.php");
 
 
 
@@ -21,30 +19,45 @@ include_once("FDL/Lib.Dir.php");
  * view spaces to administrates them
  * @param Action &$action current action
  */
-function admin(&$action,$onlymy=false) {  
-  $dbaccess = $action->GetParam("FREEDOM_DB");
+function admin( & $action, $onlymy = false)
+{
+    $dbaccess = $action->GetParam("FREEDOM_DB");
 
-  $fdoc=new_doc($dbaccess,"MAILBOX");
-  $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/AnchorPosition.js"); 
-  $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/geometry.js");
-  $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/resizeimg.js");
+    $fdoc = new_doc($dbaccess, "MAILBOX");
+    $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/AnchorPosition.js");
+    $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/geometry.js");
+    $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/resizeimg.js");
 
 
 
-  $filter=array();
-  if ($onlymy) $filter[]="owner=".intval($action->user->id);
+    $filter = array ();
+    if ($onlymy)
+    {
+        $filter[] = "owner=".intval($action->user->id);
+    }
 
-  $ls = getChildDoc($dbaccess, 0 ,0,"ALL", $filter, $action->user->id, "TABLE","MAILBOX");
-  foreach ($ls as $k=>$v) {
-    $ls[$k]["ICON"]=$fdoc->getIcon($v["icon"]);
-  }
+    $ls = getChildDoc($dbaccess, 0, 0, "ALL", $filter, $action->user->id, "TABLE", "MAILBOX");
+    foreach ($ls as $k=>$v)
+    {
+        $ls[$k]["ICON"] = $fdoc->getIcon($v["icon"]);
+    }
 
-  $action->lay->setBlockData("SPACES",$ls);
-  $action->lay->set("ficon",$fdoc->geticon());
+    $action->lay->setBlockData("SPACES", $ls);
+    $action->lay->set("ficon", $fdoc->geticon());
+
+    $doc = createDoc($dbaccess, "MAILBOX");
+    if ($doc === false)
+    {
+        $action->lay->set("create", false);
+    } else
+    {
+        $action->lay->set("create", true);
+    }
 
 }
 
-function mymailbox(&$action) {
-  admin($action,true);
+function mymailbox( & $action)
+{
+    admin($action, true);
 }
 ?>
