ac3205f81f76bb4c7d3903d020e248b3e68fa0fe
Eric Brison <eric.brison@anakeen.com>
2009-10-23 12:13:13 +0200

    simplify profil access for messages (refs #484)

---
+++ Actions/mb_profilactivate.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Activate profil for messages
+ *
+ * @author Anakeen 2009
+ * @version $Id:  $
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package MAILCONNECTOR
+ * @subpackage 
+ */
+ /**
+ */
+
+
+
+include_once("FDL/Class.Doc.php");
+
+/**
+ * Retrieve messages from IMAP folder
+ * @param Action &$action current action
+ * @global id Http var : folder mailbox identificator 
+ */
+function mb_profilactivate(&$action) {
+
+  // Get all the params      
+  $docid=GetHttpVars("id"); 
+  $dbaccess = $action->GetParam("FREEDOM_DB");
+
+  $doc = new_Doc($dbaccess, $docid);
+  if (! $doc->isAlive()) $action->exitError(sprintf(_("cannot see unknow reference %s"),$docid));
+  
+  $pdocid=$doc->getValue("fld_pdocid");
+  if ($pdocid) {
+    $pdoc=new_doc($dbaccess,$pdocid);
+    if ($pdoc->isAlive()) {
+      if ($pdoc->profid==0) {
+	$pdoc->setControl();	
+      }
+    } else $action->exitError(_("no message profil document has been defined"));
+  }
+   
+
+  redirect($action,GetHttpVars("redirect_app","FREEDOM"),
+	   GetHttpVars("redirect_act","FREEDOM_GACCESS&id=$pdocid"),
+	   $action->GetParam("CORE_STANDURL"));
+}
+?>
